71d984bc678eb7f52ea4460096dd53f05833d618
unknown <lab1064@etecjk.local>
2026-08-24 21:21:19 -0300

    Adicionando currículo do Gabriel

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
   <body>
     <hgroup>
     <h1>Currículos do grupo.</h1>
-    <p><a href="curriculoDavy.html">Currículo Davy.</a></p>
-    <p><a href="curriculoCristhian.html">Currículo Cristhian.</a></p>
+    <p><a href="curriculoDavy.html">Currículo Davy</a></p>
+    <p><a href="curriculoCristhian.html">Currículo Cristhian</a></p>
+    <p><a href="curriculoGabriel.html">Currículo Gabriel</a></p>
   </body>
   </html>
