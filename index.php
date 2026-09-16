b978b8155b470cdad74dbe7c8a401077887c5295
Davy Lima <davy.pereira@aluno.cps.sp.gov.br>
2026-09-15 22:18:05 -0300

    Adicionando menu/navbar com o Bootstrap

--- index.php
+++ index.php
@@ -1,16 +1,52 @@
   <!DOCTYPE html>
   <html lang="pt-br">
+
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Currículo</title>
   </head>
+
   <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">PegarOuLargar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Roupas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Casa e acessórios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Jogos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Esportes</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Materiais e Construção</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
     <hgroup>
-    <h1>Currículos do grupo.</h1>
-    <p><a href="curriculoDavy.html">Currículo Davy</a></p>
-    <p><a href="curriculoCristhian.html">Currículo Cristhian</a></p>
-    <p><a href="curriculoGabriel.html">Currículo Gabriel</a></p>
+      <h1>Currículos do grupo.</h1>
+      <p><a href="curriculoDavy.html">Currículo Davy</a></p>
+      <p><a href="curriculoCristhian.html">Currículo Cristhian</a></p>
+      <p><a href="curriculoGabriel.html">Currículo Gabriel</a></p>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
+
   </html>
